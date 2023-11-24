8ce38a723e0cb18a0af0b4b599780dc08f46eee2
Tobias Oberrauch <tobias.oberrauch@atos.net>
2023-11-24 15:41:58 +0100

    update

--- to_gold.py
+++ to_gold.py
@@ -1,29 +1,26 @@
 import json
 import os
-import wave
-
+from pydub import AudioSegment
 from vosk import KaldiRecognizer, Model
-
-
 
 def use_vosk(file_path, model_path):
     model = Model(model_path)
-    with wave.open(file_path, "rb") as wf:
-        recognizer = KaldiRecognizer(model, wf.getframerate())
-        while True:
-            data = wf.readframes(4000)
-            if len(data) == 0:
-                break
-            if recognizer.AcceptWaveform(data):
-                pass
-        return json.loads(recognizer.FinalResult())['text']
+    audio = AudioSegment.from_mp3(file_path).set_frame_rate(16000).set_channels(1)
+    recognizer = KaldiRecognizer(model, 16000)
+
+    chunk_size = 4000  # Process 4 seconds at a time
+    for i in range(0, len(audio), chunk_size):
+        chunk = audio[i:i+chunk_size].raw_data
+        recognizer.AcceptWaveform(chunk)
+    
+    return json.loads(recognizer.FinalResult())['text']
 
 def process_directory(source_dir, target_dir, model_path):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
     for filename in os.listdir(source_dir):
-        if filename.lower().endswith('.wav'):
+        if filename.lower().endswith('.mp3'):
             file_path = os.path.join(source_dir, filename)
             result = use_vosk(file_path, model_path)
             target_file_path = os.path.join(target_dir, os.path.splitext(filename)[0] + '.txt')
@@ -31,7 +28,7 @@ def process_directory(source_dir, target_dir, model_path):
             with open(target_file_path, 'w') as f:
                 f.write(result)
 
-source_dir = 'data/silver'
+source_dir = 'data/silver_mini'
 target_dir = 'data/gold'
 model_path = 'models/vosk-model-en-us-0.22/vosk-model-en-us-0.22'
 process_directory(source_dir, target_dir, model_path)
--- to_silver.py
+++ to_silver.py
@@ -7,12 +7,12 @@ def extract_audio_from_video(video_path, output_audio_path):
         os.makedirs(os.path.dirname(output_audio_path), exist_ok=True)
         
         video = VideoFileClip(video_path)
-        video.audio.write_audiofile(output_audio_path)
+        # Specify codec for MP3 and set the file extension to .mp3
+        video.audio.write_audiofile(output_audio_path, codec='libmp3lame')
         return True
     except Exception as e:
         print(f"Error extracting audio from {video_path}: {e}")
         return False
-
 
 directory = 'data/bronze'
 extensions = ['.3gpp', '.mp4']
@@ -21,8 +21,9 @@ for root, dirs, files in os.walk(directory):
     for file in files:
         if file.endswith(tuple(extensions)):
             file_path = os.path.join(root, file)
-            wav_path = f'data/silver/{os.path.splitext(file)[0]}.wav'
-            if extract_audio_from_video(file_path, wav_path):
-                print(f"Audio extracted to {wav_path}")
+            # Change the file extension to .mp3
+            mp3_path = f'data/silver/{os.path.splitext(file)[0]}.mp3'
+            if extract_audio_from_video(file_path, mp3_path):
+                print(f"Audio extracted to {mp3_path}")
             else:
                 print(f"Failed to extract audio for {file_path}")
